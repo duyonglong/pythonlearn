9207ece90b5535908c30a69ad754f7aae5405dca
duyonglong <2695123770@qq.com>
2020-09-24 19:13:21 +0800

    threeproject

---
+++ threeproject/ascii_code.py
@@ -0,0 +1,16 @@
+# _*_ coding: UTF-8 _*_
+# 开发团队: 个人
+# 开发人员: ritchie
+# 开发时间: 2020/9/24 18:14
+# 文件名称: ascii_code.py
+# 开发工具: PyCharm
+
+"""
+job:1
+功能：在键盘输入相应的字母、数字、或符号，输出其ASCII的状态值，即十进制的数字值。
+"""
+
+var_char = input("请输入:")
+print("\n")
+print(var_char+" 的ASCII编码:", ord(var_char))
+
---
+++ threeproject/my_dream.py
@@ -0,0 +1,23 @@
+# _*_ coding: UTF-8 _*_
+# 开发团队: 个人
+# 开发人员: ritchie
+# 开发时间: 2020/9/24 18:37
+# 文件名称: my_dream.py
+# 开发工具: PyCharm
+
+"""
+job:3
+功能:打印出自己向往的生活愿意
+"""
+
+var_char1 = input("请输入自己向往的生活\n")
+var_char2 = input()
+var_char3 = input()
+var_char4 = input()
+print("\n")
+print(var_char1)
+print(var_char2)
+print(var_char3)
+print(var_char4)
+
+
