f7fe019a5ae7d8561fe77cda9792ab534410768f
duyonglong <2695123770@qq.com>
2020-09-24 16:12:56 +0800

    first commit

---
+++ oneproject/go_big.py
@@ -0,0 +1,14 @@
+# _*_ coding: UTF-8 _*_
+# 开发团队: 个人
+# 开发人员: ritchie
+# 开发时间: 2020/9/24 10:41
+# 文件名称: go_big.py
+# 开发工具: PyCharm
+
+"""
+job:1
+
+输出：Go Big Or Go Home!
+
+"""
+print("Go Big Or Go Home!")
---
+++ twoproject/eat_food.py
@@ -0,0 +1,16 @@
+# _*_ coding: UTF-8 _*_
+# 开发团队: 个人
+# 开发人员: ritchie
+# 开发时间: 2020/9/24 14:27
+# 文件名称: eat_food.py
+# 开发工具: PyCharm
+
+"""
+job:1
+输出：eat our own dog food
+"""
+print("   想要满足用户需求吗？   ")
+print("           |           ")
+print("-----------------------")
+print("|eat our own dog food!|")
+print("-----------------------")
